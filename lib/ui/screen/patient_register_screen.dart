c30b716ac40e41fadd34f9722147637d80669a91
Duban-sg <universidad.duban@gmail.com>
2021-05-02 00:32:42 -0500

    guarda foto

--- lib/ui/screen/patient_register_screen.dart
+++ lib/ui/screen/patient_register_screen.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:parcial_two/model/patient_model.dart';
 import 'package:parcial_two/repository/patient_repository.dart';
 import 'package:parcial_two/ui/widget/button_generic.dart';
 import 'package:parcial_two/ui/widget/text_field.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PatientRagister extends StatefulWidget {
   @override
@@ -21,6 +27,7 @@ class _PatientRagister extends State<PatientRagister> {
   TextEditingController ctrlNeighborhood;
   TextEditingController ctrlPhone;
   TextEditingController ctrlCity;
+  File Imagen;
 
   @override
   void initState() {
@@ -51,6 +58,7 @@ class _PatientRagister extends State<PatientRagister> {
             SizedBox(
               height: 5.0,
             ),
+            imagenSelect(context),
             MyTextField(
               myController: ctrlPatientId,
               myLabel: 'Identificacion',
@@ -73,7 +81,7 @@ class _PatientRagister extends State<PatientRagister> {
                   status: 'Active',
                   name: ctrlName.text,
                   lastName: ctrlLastName.text,
-                  photo: 'Sinfoto',
+                  photo: Imagen==null?null:imageToBase64(Imagen),
                   age: ctrlAge.text,
                   address: ctrlAddress.text,
                   neighborhood: ctrlNeighborhood.text,
@@ -97,5 +105,91 @@ class _PatientRagister extends State<PatientRagister> {
         ),
       ),
     );
+  }
+
+  imagenSelect(context) {
+    return RaisedButton(
+      onPressed: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              _showChoiceDialog(context);
+            },
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Image(
+                image: Imagen != null
+                    ? FileImage(Imagen, scale: 0.1)
+                    : AssetImage('assets/No_image.png'),
+                fit: BoxFit.cover,
+                height: 180,
+                width: 280,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _openGallery(BuildContext context) async {
+    var picture = await ImagePicker().getImage(source: ImageSource.gallery);
+    this.setState(() {
+      Imagen = File(picture.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async {
+    var picture = await ImagePicker().getImage(source: ImageSource.camera);
+    this.setState(() {
+      Imagen = File(picture.path);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              title: Text("Elegir foto"),
+              content: SingleChildScrollView(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  FlatButton(
+                      onPressed: () {
+                        _openGallery(context);
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.photo_library),
+                          Text('  Elegir photo de la galeria'),
+                        ],
+                      )),
+                  FlatButton(
+                      onPressed: () {
+                        _openCamera(context);
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.camera_alt_sharp),
+                          Text('  Haz una foto'),
+                        ],
+                      ))
+                ],
+              )));
+        });
+  }
+
+  imageToBase64(File file) {
+    List<int> imageBytes = file.readAsBytesSync();
+    return base64Encode(imageBytes);
   }
 }
